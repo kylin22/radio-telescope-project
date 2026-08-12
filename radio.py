60ad83d2398074e000265991939647fe4bd792bd
kylin22 <paulshen23@gmail.com>
2026-08-12 16:04:51 +1000

    fix: IQ and phase plots + code formatting

--- radio.py
+++ radio.py
@@ -5,18 +5,19 @@ from rtlsdr import RtlSdr
 
 # SDR setup
 sdr = RtlSdr()
-sdr.sample_rate = 2.4e6 # controls span of data 
-sdr.center_freq = 100e6 # FM range 
+sdr.sample_rate = 1.2e6 # controls span of data 
+sdr.center_freq = 100.8 * 1e6 # FM range 
 sdr.gain = "auto"
 
 # Options
 FFT_SIZE = 8192 # the frequency resolution is sample_rate / FFT_SIZE
 WATERFALL_ROWS = 120 # how much history to keep on screen
-POWER_MIN = -50.0 # should be around the noise floor (min estimate of noise power in dB)
-POWER_MAX = 50
+POWER_MIN = -120.0 # should be around the noise floor (min estimate of noise power in dB)
+POWER_MAX = -40.0
 UPDATE_INTERVAL = 50
 
 window = np.hanning(FFT_SIZE) # filter out signal leakage across bins
+# assign frequencies for each bin
 freqs_raw = np.fft.fftshift(np.fft.fftfreq(FFT_SIZE, d=1 / sdr.sample_rate) + sdr.center_freq)
 freqs = freqs_raw / 1e6 # MHz 
 
@@ -30,7 +31,7 @@ waterfall = np.full((WATERFALL_ROWS, FFT_SIZE), -160.0)
 spectrum_plot = pg.PlotWidget(labels={"left": "PSD (dB/Hz)", "bottom": "Frequency (MHz)"})
 spectrum_plot.setMouseEnabled(x=False, y=False) # get rid of default dragging functionality
 curve = spectrum_plot.plot(freqs, waterfall[-1], pen=pg.mkPen("y", width=1))
-spectrum_plot.setYRange(-160.0, -40.0)
+spectrum_plot.setYRange(POWER_MIN, POWER_MAX)
 spectrum_plot.getPlotItem().enableAutoRange(False, False)
 
 # adding spectrum plot to layout
@@ -61,7 +62,7 @@ waterfall_row.addWidget(waterfall_plot)
 colorbar = pg.HistogramLUTWidget()
 colorbar.setImageItem(image)
 colorbar.item.gradient.loadPreset("plasma") 
-image.setLevels((-30, 20)) # needs to come after colorbar is created for some reason
+image.setLevels((POWER_MIN, POWER_MAX)) # needs to come after colorbar is created for some reason
 colorbar.orientation = "vertical"
 colorbar.setFixedWidth(120)
 waterfall_row.addWidget(colorbar)
@@ -71,6 +72,9 @@ layout.addLayout(waterfall_row)
 iq_plot = pg.PlotWidget(labels={"left": "Q", "bottom": "I"})
 iq_plot.setMouseEnabled(x=False, y=False)
 iq_plot.setAspectLocked(True, ratio=1)
+iq_plot.getPlotItem().enableAutoRange(False, False)
+iq_plot.setXRange(-1.0, 1.0)
+iq_plot.setYRange(-1.0, 1.0)
 iq_scatter = pg.ScatterPlotItem(size=2, pen=None, brush=pg.mkBrush(0, 255, 0, 120))
 iq_plot.addItem(iq_scatter)
 
@@ -87,8 +91,7 @@ layout.addLayout(iq_phase_row)
 win.setWindowTitle("RTL-SDR Spectrum")
 win.showMaximized()
 
-def get_power_spectral_density():
-    samples = sdr.read_samples(FFT_SIZE) # list of complex IQ samples
+def power_spectral_density(samples):
     spectrum = np.fft.fft(samples * window)
 
     # psd estimate (power per Hz)
@@ -100,11 +103,26 @@ def get_power_spectral_density():
 
 def update():
     global waterfall
-    psd = get_power_spectral_density()
+
+    samples = sdr.read_samples(FFT_SIZE)
+    # calculate data for IQ and phase plots
+    i = np.real(samples)
+    q = np.imag(samples)
+    phase = np.angle(samples)
+    time = np.arange(samples.size) / sdr.sample_rate
+
+    # update plots
+    iq_scatter.setData(i, q)
+    iq_plot.setXRange(-1.0, 1.0) # fixes axes from autoscaling every frame
+    iq_plot.setYRange(-1.0, 1.0)
+    phase_curve.setData(time, phase)
+
+    psd = power_spectral_density(samples)
     curve.setData(freqs, psd)
     waterfall = np.roll(waterfall, 1, axis=0) # push waterfall row down
     waterfall[0, :] = psd # set waterfall row to current data
     image.setImage(waterfall, autoLevels=False)
+
 
 
 timer = QtCore.QTimer()
